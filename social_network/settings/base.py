e08f5227005d2bc338678faf658c8dd349247de7
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-12-07 14:51:11 +0100

    add STATICFILES_DIRS

--- social_network/settings/base.py
+++ social_network/settings/base.py
@@ -154,6 +154,9 @@ GRAPHQL_JWT = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
