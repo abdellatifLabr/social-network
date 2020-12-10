6c4b6ec800f5336195ef21330d8670c3edc55c3d
Labreche Abdellatif <tifo.zelo@gmail.com>
2020-12-10 18:19:08 +0100

    ...

--- social_network/asgi.py
+++ social_network/asgi.py
@@ -1,5 +1,6 @@
 import os
 
+import django
 from channels.routing import get_default_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -7,7 +8,7 @@ from channels.auth import AuthMiddlewareStack
 from social_network.routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
     'http': get_default_application(),
     'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
